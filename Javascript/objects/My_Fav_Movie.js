3a93f2354f77654679f2546805b117a0288a4cd4
Robert Gabriel <robert_gabriel@outlook.com>
2016-02-09 14:33:25 +0000

    Cleaner example

--- Javascript/objects/My_Fav_Movie.js
+++ Javascript/objects/My_Fav_Movie.js
@@ -1,18 +1,11 @@
 var myFavMovie = {
     title: 'Die Hard',
     duration: 30,
-    stars: ['Bruice Willis ', 'Batman']
+    stars: ['Bruice Willis', ' Batman']
 };
 
 function printMovie(movie) {
-    console.log(movie.title + ' It is  ' + movie.duration + ' minutes long ');
-    var starsString = 'Stars: ';
-    for (var i = 0; i < movie.stars.length; i++) {
-        starsString += movie.stars[i];
-        if (i != movie.stars.length -1) {
-            starsString += ', ';
-        }
-    }
-    console.log(starsString);
+    console.log(movie.title + '\nIt is  ' + movie.duration + ' minutes long');
+    console.log('It stars ' + movie.stars.join('along side'));
 }
 printMovie(myFavMovie);
