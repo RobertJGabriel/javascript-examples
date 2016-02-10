03c59e262f7168a378acd38dacb5def557f66f39
Robert Gabriel <robert_gabriel@outlook.com>
2016-02-10 10:42:04 +0000

    Added Handle example

---
+++ Handlebars/js/core.js
@@ -0,0 +1,35 @@
+$(window).load(function(){
+var source = $("#some-template").html(); 
+var template = Handlebars.compile(source); 
+
+var data = { 
+    users: [ { 
+        person: {
+            firstName: "Robert", 
+            lastName: "Gabriel"
+        },
+        jobTitle: "Leggo",
+        twitter: "wobertg" 
+    }, { 
+        person: {
+            firstName: "Wobert", 
+            lastName: "Gabriel"
+        },
+        jobTitle: "Leggo",
+        twitter: "wobertg" 
+    }, { 
+        person: {
+            firstName: "Zobert", 
+            lastName: "Gabriel"
+        },
+        jobTitle: "Leggo",
+        twitter: "wobertg" 
+    }]
+}; 
+
+Handlebars.registerHelper('fullName', function(person) {
+  return person.firstName + " " + person.lastName;
+});
+
+$('#middle').append(template(data));
+});//]]> 
